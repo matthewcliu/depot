1ccfb0f25177bcc32b147b97fd6c42817c3db878
Matthew Liu <matthewliu@gmail.com>
2011-07-13 00:36:10 -0700

    Initial Depot Scaffold, Seed Data, and CSS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'd7f1fa1ecac4b56b6e2dcb5001e6e200fc8aebd215bca8fd1eb4c7ab266e685fa70d4eb7f88776d91c624db179986021119cea834a2250cebb67e98dbe1a5edc'
